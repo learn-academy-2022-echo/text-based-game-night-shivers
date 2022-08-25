ffc8de46b50ce7953500b4b6789e2125b744bca7
kellychan56 <kelly.chan568@gmail.com>
2022-08-25 10:22:49 -0700

    initial game

---
+++ night-shivers.rb
@@ -0,0 +1,4 @@
+
+
+
+ puts "You wake up in the middle of the night, and it's pitch black. You see nothing a pair of white eyes in the corner. You don't know where you are, you can’t see anything, you’re confused and scared like a little baby. The pair of white eyes slowly move towards you and you can hear the floorboard creaking."
